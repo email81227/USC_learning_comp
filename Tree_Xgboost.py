a5435a022d692cdf44a9dd8622c5d3ccd90f8fd0
Lei <email81227@gmail.com>
2018-05-02 09:45:51 +0100

    2018-04-23 split project from courseworks.
    2018-04-30 Add Xgboost.
    
    2018-05-02 tune Xgboost.

--- Tree_Xgboost.py
+++ Tree_Xgboost.py
@@ -3,6 +3,7 @@ import pdb
 import xgboost as xgb
 
 from sklearn.preprocessing import LabelEncoder
+from xgboost.sklearn import XGBClassifier
 
 # Customized functions
 from PublicFunctions import *
@@ -29,25 +30,33 @@ X_ts = X_ts.reshape((X_ts.shape[0], X_ts.shape[1] * X_ts.shape[2]))
 lb = LabelEncoder()
 y_tr = lb.fit_transform(y_tr)
 
-dtrain = xgb.DMatrix(X_tr, label=y_tr)
+# dtrain = xgb.DMatrix(X_tr, label=y_tr)
 dtest = xgb.DMatrix(X_ts)
-
-# xgboost
-param = {
-    'max_depth': 10,  # the maximum depth of each tree
-    'eta': 0.2,  # the training step for each iteration
-    'silent': 1,  # logging mode - quiet
-    'objective': 'multi:softprob',  # error evaluation for multiclass training
-    'num_class': 10}  # the number of classes that exist in this datset
-num_round = 200  # the number of training iterations
-
-bst = xgb.train(param, dtrain, num_round)
+#
+# # xgboost
+# param = {
+#     'max_depth': 10,  # the maximum depth of each tree
+#     'eta': 0.2,  # the training step for each iteration
+#     'silent': 1,  # logging mode - quiet
+#     'objective': 'multi:softprob',  # error evaluation for multiclass training
+#     'num_class': 10}  # the number of classes that exist in this datset
+# num_round = 200  # the number of training iterations
+#
+# bst = xgb.train(param, dtrain, num_round)
 # bst.dump_model('dump.raw.txt')
-
-
-preds = bst.predict(dtest)
-prediction = np.asarray([np.argmax(line) for line in preds])
+# preds = bst.predict(dtest)
+# prediction = np.asarray([np.argmax(line) for line in preds])
 # pdb.set_trace()
 
+xgb_model = XGBClassifier(learning_rate=0.01, n_estimators=200, max_depth=9, eta=2, silent=1,
+                          min_child_weight=3, gamma=0.2, subsample=0.85, colsample_bytree=0.75,
+                          objective='multi:softprob', nthread=4, scale_pos_weight=1, num_class=10,
+                          reg_alpha=0.0001)
+
+# pdb.set_trace()
+
+xgb_model.fit(X_tr, y_tr)
+prediction = xgb_model.predict(X_ts)
+
 test['Class'] = list(lb.inverse_transform(prediction))
-test.to_csv(join(r'D:\DataSet\UrbanSoundChallenge\submission', 'sub_xgb_256_r200.csv'), index=False)
+test.to_csv(join(r'D:\DataSet\UrbanSoundChallenge\submission', 'sub_xgb_tuned.csv'), index=False)
--- Tune _Xgboost.py
+++ Tune _Xgboost.py
@@ -33,10 +33,11 @@ lb = LabelEncoder()
 y_tr = lb.fit_transform(y_tr)
 
 
-def xgboost_param_selection(param_grid, Xtr, ytr, Xts, sample_weight=None, nfolds=5):
-    model = XGBClassifier(learning_rate=0.1, n_estimators=20, max_depth=5, eta=2, silent=1,
-                          min_child_weight=1, gamma=0, subsample=0.8, colsample_bytree=0.8,
-                          objective='multi:softprob', nthread=4, scale_pos_weight=1, num_class=10)
+def xgboost_param_selection(param_grid, Xtr, ytr, Xts, sample_weight=None, nfolds=3):
+    model = XGBClassifier(learning_rate=0.01, n_estimators=20, max_depth=9, eta=2, silent=1,
+                          min_child_weight=3, gamma=0.2, subsample=0.85, colsample_bytree=0.75,
+                          objective='multi:softprob', nthread=4, scale_pos_weight=1, num_class=10,
+                          reg_alpha=0.0001)
     gr_search = GridSearchCV(model, param_grid, cv=nfolds, n_jobs=1, iid=False)
     gr_search.fit(Xtr, ytr, sample_weight)
 
@@ -44,14 +45,24 @@ def xgboost_param_selection(param_grid, Xtr, ytr, Xts, sample_weight=None, nfold
     return gr_search.predict(Xts), gr_search.cv_results_
 
 
-dtrain = xgb.DMatrix(X_tr, label=y_tr)
-dtest = xgb.DMatrix(X_ts)
-
 # xgboost
-max_depth = range(6, 10, 2)         # list(np.logspace(5, 15, 11))
-min_child_weight = range(1, 3, 2)   # list(np.logspace(-9, 1, 11))
-# degrees = [3, 4, 5]
-param_grid = {'max_depth': max_depth, 'min_child_weight': min_child_weight}  # , 'degree': degrees}
+# {'max_depth': 9, 'min_child_weight': 3, 'gamma': 0.2, 'colsample_bytree': 0.75, 'subsample': 0.85,
+# 'reg_alpha': 1e-05}
+
+# max_depth = [7, 8, 9]          # range(7, 10, 1)
+# min_child_weight = [1, 2, 3]   # range(1, 3, 2)
+# param_grid = {'max_depth': max_depth, 'min_child_weight': min_child_weight}
+
+# gamma = [i/10.0 for i in range(4, 6)]
+# param_grid = {'gamma': gamma}
+
+# subsample = [i/100.0 for i in range(75, 90, 5)]
+# colsample_bytree = [i/100.0 for i in range(75, 90, 5)]
+# param_grid = {'subsample': subsample, 'colsample_bytree': colsample_bytree}
+
+# reg_alpha = [1e-7, 1e-6, 1e-5, 1e-4]
+reg_alpha = [1e-5]
+param_grid = {'reg_alpha': reg_alpha}
 
 # param = {
 #     'max_depth': 25,  # the maximum depth of each tree
@@ -71,7 +82,7 @@ param_grid = {'max_depth': max_depth, 'min_child_weight': min_child_weight}  # ,
 
 prediction, tune_result = xgboost_param_selection(param_grid, X_tr, y_tr, X_ts)
 tune_result = pd.DataFrame(tune_result)
-tune_result.to_csv(join(r'D:\DataSet\UrbanSoundChallenge\submission', 'xgb_tune_results.csv'), index=False)
+tune_result.to_csv(join(r'D:\DataSet\UrbanSoundChallenge\submission', 'xgb_tune6_results.csv'), index=False)
 
 test['Class'] = list(lb.inverse_transform(prediction))
-test.to_csv(join(r'D:\DataSet\UrbanSoundChallenge\submission', 'sub_xgb_r200.csv'), index=False)
+test.to_csv(join(r'D:\DataSet\UrbanSoundChallenge\submission', 'sub_xgb_tune6.csv'), index=False)
