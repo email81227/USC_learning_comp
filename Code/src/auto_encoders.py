777b9db671b5f2d9b23f72b32f3a15310420784f
ding <email81227@gmail.com>
2019-12-16 01:04:11 +0800

    Finding problem of AutoEncoders

--- Code/src/auto_encoders.py
+++ Code/src/auto_encoders.py
@@ -7,40 +7,37 @@ import time
 
 LATENT_SHAPE = 32
 
+# TODO: Check why performance differnet between Keras + TF1.X and TF2.0
+
 
 class Encoder(tf.keras.layers.Layer):
     '''
     As part of auto-encoder, inherit from Layer makes sense
     '''
-    def __init__(self, target_dim):
+    def __init__(self, input_dim, target_dim):
         super(Encoder, self).__init__()
-        self.output_layer = tf.keras.layers.Dense(
-            units=target_dim,
-            activation='relu'
-        )
+        self.inputs_layer = tf.keras.layers.Input(shape=input_dim)
+        self.hidden_layer = tf.keras.layers.Dense(units=target_dim, activation='relu')
 
     def call(self, samples):
-        # activation = self.hidden_layer(input_features)
-        return self.output_layer(samples)
+        return self.hidden_layer(self.inputs_layer(samples))
 
 
 class Decoder(tf.keras.layers.Layer):
     def __init__(self, input_dim, target_dim):
         super(Decoder, self).__init__()
-        self.output_layer = tf.keras.layers.Dense(
-            units=input_dim,
-            activation='sigmoid'
-        )
+        self.inputs_layer = tf.keras.layers.Input(shape=target_dim)
+        self.hidden_layer = tf.keras.layers.Dense(units=input_dim, activation='sigmoid')
 
-    def call(self, encode):
-        return self.output_layer(encode)
+    def call(self, encoded):
+        return self.hidden_layer(self.inputs_layer(encoded))
 
 
-class Autoencoder(tf.keras.Model):
+class AutoEncoder(tf.keras.Model):
     def __init__(self, input_shape, target_dim=LATENT_SHAPE):
-        super(Autoencoder, self).__init__()
-        self.encoder = Encoder(target_dim=target_dim)
-        self.decoder = Decoder(input_shape, target_dim=target_dim)
+        super(AutoEncoder, self).__init__()
+        self.encoder = Encoder(input_shape, target_dim)
+        self.decoder = Decoder(input_shape, target_dim)
 
         self.model = tf.keras.models.Sequential()
         self.model.add(self.encoder)
@@ -73,7 +70,7 @@ if __name__ == '__main__':
     print(tr_X.shape)
     print(ts_X.shape)
     
-    model = Autoencoder(784)
+    model = AutoEncoder((784,), 256)
     model.compile(optimizer='adadelta', loss='binary_crossentropy')
     model.fit(tr_X, tr_X,
               epochs=50,
